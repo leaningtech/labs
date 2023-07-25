db12b8f02d48da85bb48f183c6d286b5bac03248
Alex Bates <alex@nanaian.town>
2023-07-25 16:11:56 +0100

    independently-scrollable sidebar

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,8 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+interface Element {
+	// Non-standard. Not supported on Firefox.
+	// https://developer.mozilla.org/en-US/docs/Web/API/Element/scrollIntoViewIfNeeded
+	scrollIntoViewIfNeeded(centerIfNeeded?: boolean): void;
+}
