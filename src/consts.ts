da118c8a4d23c76b3d579b05d6932dfe8efe0038
Ana Gabriela Reyna <astromeria@Anas-MacBook-Pro.local>
2023-08-10 18:17:34 +0100

    footer improved, removed sections, hero buttons

--- src/consts.ts
+++ src/consts.ts
@@ -2,3 +2,6 @@ export const SITE_TITLE = "Leaning Technologies Developer";
 export const SITE_DESCRIPTION = ""; // TODO
 
 export const DISCORD_URL = "https://discord.gg/qBMHpK9Kqv";
+export const GITHUB_URL = "https://github.com/leaningtech";
+export const TWITTER_URL = "https://twitter.com/leaningtech";
+export const LINKEDIN_URL = "https://www.linkedin.com/company/leaning-technologies-ltd/";
