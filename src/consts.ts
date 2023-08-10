23ac1e0978b48d75f8cdaf7505ba9c0cb9e51750
Ana Gabriela Reyna <astromeria@Anas-MacBook-Pro.local>
2023-08-10 18:24:24 +0100

    prettier formatting

--- src/consts.ts
+++ src/consts.ts
@@ -4,4 +4,5 @@ export const SITE_DESCRIPTION = ""; // TODO
 export const DISCORD_URL = "https://discord.gg/qBMHpK9Kqv";
 export const GITHUB_URL = "https://github.com/leaningtech";
 export const TWITTER_URL = "https://twitter.com/leaningtech";
-export const LINKEDIN_URL = "https://www.linkedin.com/company/leaning-technologies-ltd/";
+export const LINKEDIN_URL =
+	"https://www.linkedin.com/company/leaning-technologies-ltd/";
