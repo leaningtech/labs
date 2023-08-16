f13f5551cbbe8091e52206ade367f8e839b29cad
Ana Gabriela Reyna <gabrielareynaflores@gmail.com>
2023-08-16 16:39:30 +0100

    smaller product logo, horizontal doodle for mobile (#6)

--- src/components/landing-page/Doodle.svelte
+++ src/components/landing-page/Doodle.svelte
@@ -1,8 +1,13 @@
 <script lang="ts">
 	const images = [
-		"/landing/doodle/cheerp_top.png",
-		"/landing/doodle/cheerpj_top.png",
-		"/landing/doodle/cheerpx_top.png",
+		"/landing/doodle/doodle_vertical/cheerp_top.png",
+		"/landing/doodle/doodle_vertical/cheerpj_top.png",
+		"/landing/doodle/doodle_vertical/cheerpx_top.png",
+	];
+	const images_h = [
+		"/landing/doodle/doodle_horizontal/cheerp_h.png",
+		"/landing/doodle/doodle_horizontal/cheerpj_h.png",
+		"/landing/doodle/doodle_horizontal/cheerpx_h.png",
 	];
 
 	let index = 0;
@@ -11,9 +16,9 @@
 	}, 5000);
 </script>
 
-<div class="doodle relative w-96 md:w-full max-w-full max-h-[36rem] lg:max-h-[48rem] overflow-hidden">
+<div class="doodle relative w-96 hidden md:block  md:w-full max-w-full max-h-[36rem] lg:max-h-[48rem] overflow-hidden">
 	<img
-		src="/landing/doodle/all_top.png"
+		src="/landing/doodle/doodle_vertical/all_top.png"
 		alt=""
 		class="absolute top-0 opacity-20"
 	/>
@@ -27,7 +32,30 @@
 		/>
 	{/each}
 	<img
-	src="/landing/doodle/browser.png"
+	src="/landing/doodle/doodle_vertical/browser.png"
+	alt=""
+	class="absolute top-0"
+	/>
+
+</div>
+
+<div class="doodle_h relative w-full md:hidden mt-28">
+	<img
+		src="/landing/doodle/doodle_horizontal/all_top_h.png"
+		alt=""
+		class="absolute top-0 opacity-20"
+	/>
+
+	{#each images_h as image, i}
+		<img
+			src={image}
+			alt=""
+			class="absolute top-0 transition-opacity duration-500"
+			class:opacity-0={i !== index}
+		/>
+	{/each}
+	<img
+	src="/landing/doodle/doodle_horizontal/browser_h.png"
 	alt=""
 	class="absolute top-0"
 	/>
@@ -39,4 +67,8 @@
 	.doodle {
 		aspect-ratio: 1101 / 1577;
 	}
+	.doodle_h {
+		aspect-ratio: 1874 / 1101;
+	}
+	
 </style>
