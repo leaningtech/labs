8540f2523436fe8fec24c933d6fa558deef54eb5
Alex Bates <alex@bates64.com>
2024-06-26 14:46:54 +0100

    fix cheerpj site

--- sites/cheerpj/src/content/config.ts
+++ sites/cheerpj/src/content/config.ts
@@ -1,5 +1,19 @@
 import { defineDocsCollection } from "@leaningtech/astro-theme/content";
+import { defineCollection, z } from "astro:content";
 
 const docs = defineDocsCollection();
 
-export const collections = { docs };
+const showcase = defineCollection({
+	type: "data",
+	schema: ({ image }) =>
+		z.object({
+			title: z.string(),
+			description: z.string().optional(),
+			url: z.string(),
+			image: image().refine((img) => img.width / img.height == 1.5, {
+				message: "Image must have 3:2 aspect ratio",
+			}),
+		}),
+});
+
+export const collections = { docs, showcase };
--- sites/labs/src/content/config.ts
+++ sites/labs/src/content/config.ts
@@ -42,18 +42,4 @@ const blog = defineCollection({
 
 const docs = defineDocsCollection();
 
-const showcase = defineCollection({
-	type: "data",
-	schema: ({ image }) =>
-		z.object({
-			title: z.string(),
-			description: z.string().optional(),
-			url: z.string(),
-			image: image().refine((img) => img.width / img.height == 1.5, {
-				message: "Image must have 3:2 aspect ratio",
-			}),
-			tags: productTags,
-		}),
-});
-
-export const collections = { docs, blog, blogauthors, showcase };
+export const collections = { docs, blog, blogauthors };
