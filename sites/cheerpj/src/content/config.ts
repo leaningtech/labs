6806c78fd5bf71bf597e5c5233ebb77bf131ae93
Alex Bates <alex@bates64.com>
2024-07-22 10:48:35 +0100

    Multi-site monorepo (#91)

---
+++ sites/cheerpj/src/content/config.ts
@@ -0,0 +1,17 @@
+import { defineCommonCollections } from "@leaningtech/astro-theme/content";
+import { defineCollection, z } from "astro:content";
+
+const showcase = defineCollection({
+	type: "data",
+	schema: ({ image }) =>
+		z.object({
+			title: z.string(),
+			description: z.string().optional(),
+			url: z.string(),
+			image: image().refine((img) => img.width / img.height == 1.5, {
+				message: "Image must have 3:2 aspect ratio",
+			}),
+		}),
+});
+
+export const collections = { showcase, ...defineCommonCollections() };
