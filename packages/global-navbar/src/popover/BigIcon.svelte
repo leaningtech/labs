dc143853b81070f3c16e1b10809b17d7f3caff1c
Alex Bates <alex@bates64.com>
2024-08-01 12:47:42 +0100

    global-navbar fixes (#105)

--- packages/global-navbar/src/popover/BigIcon.svelte
+++ packages/global-navbar/src/popover/BigIcon.svelte
@@ -11,3 +11,35 @@
 		<span>{description}</span>
 	</a>
 </li>
+
+<style>
+	li {
+		margin: 0; /* required to override style on cheerpj.com */
+	}
+
+	a {
+		color: white;
+		text-decoration: none;
+
+		padding: 1rem;
+		border-radius: 8px;
+
+		&:global(:has(svg)) { /* <slot /> */
+			display: grid;
+			grid-template-rows: 1fr 1fr;
+			grid-template-columns: 3rem 1fr;
+			align-items: center;
+			gap: 0.5rem;
+
+			& :global(svg) {
+				grid-row: span 2;
+				width: 80%;
+				height: 100%;
+			}
+		}
+	}
+
+	span {
+		color: rgb(168 162 158);
+	}
+</style>
