7438b9cd0a822b998a174aff7b83a9671835deef
Haseeb Qureshie <haseebqureshie@gmail.com>
2024-11-11 12:48:36 +0100

    Added global CX version variable

--- packages/astro-theme/lib/code-replace.js
+++ packages/astro-theme/lib/code-replace.js
@@ -1,4 +1,13 @@
-const replacements = [];
+export let CX_LATEST = "1.0.5";
+
+const replacements = [
+	[
+		"%CX_LATEST%",
+		() => {
+			return Promise.resolve(CX_LATEST);
+		},
+	],
+];
 
 document.addEventListener("DOMContentLoaded", () => {
 	const spans = document.querySelectorAll(".expressive-code code span");
@@ -7,9 +16,32 @@ document.addEventListener("DOMContentLoaded", () => {
 		for (const [search, replace] of replacements) {
 			if (span.innerText.includes(search)) {
 				replace().then((s) => {
-					span.innerText = span.innerText.replace(search, s);
+					span.innerHTML = span.innerHTML.replace(search, s);
 				});
 			}
 		}
 	}
+
+	const badge = document.querySelectorAll("img[src*='%CX_LATEST%']");
+	badge.forEach((img) => {
+		const newSrc = img.src.replace("%CX_LATEST%", CX_LATEST);
+		img.src = newSrc;
+	});
 });
+
+// Function to update data-code before button click
+function updateDataCode() {
+	// Find button with data-code attribute (``` copy button)
+	document.querySelectorAll("[data-code]").forEach((btn) => {
+		let originalCode = btn.dataset.code;
+
+		// Replace the placeholder %CX_LATEST% with the newest versions of CX
+		let replacedCode = originalCode.replace(/%CX_LATEST%/g, CX_LATEST);
+
+		// Update the button's data-code attribute with the modified data
+		btn.dataset.code = replacedCode;
+	});
+}
+
+// When page loads, call the function to update the code blocks
+window.addEventListener("DOMContentLoaded", updateDataCode);
