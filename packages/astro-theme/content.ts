04a72db1fbe81b867c8e05e92b3ad13ea841fa00
Ana Gabriela Reyna <gabrielareynaflores@gmail.com>
2025-05-23 16:53:16 +0100

    added showcase as a global collection

--- packages/astro-theme/content.ts
+++ packages/astro-theme/content.ts
@@ -51,5 +51,17 @@ export function defineCommonCollections() {
 					tags: productTags.optional(),
 				}),
 		}),
+		showcase: defineCollection({
+			type: "data",
+			schema: ({ image }) =>
+				z.object({
+					title: z.string(),
+					description: z.string().optional(),
+					url: z.string(),
+					heroImage: image().refine((img) => img.width / img.height == 1.5, {
+						message: "Image must have 3:2 aspect ratio",
+					}),
+				}),
+		}),
 	};
 }
