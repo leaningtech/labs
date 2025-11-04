8439a1873358aae092ed98c6503d7aa30c8bf40d
Yuri Iozzelli <y.iozzelli@gmail.com>
2025-11-04 18:43:22 +0100

    rework variable replacement logic
    We now do it at built time, using remark and rehype plugins.

---
+++ packages/astro-theme/replace-variables.ts
@@ -0,0 +1,49 @@
+import { visit } from "unist-util-visit";
+
+const replacements = {
+	CX_LATEST: "1.1.5",
+	BP_LATEST: "1.0",
+};
+
+export function remarkReplaceVars() {
+	return (tree) => {
+		visit(tree, ["text", "code", "inlineCode"], (node) => {
+			if (node.value) {
+				Object.entries(replacements).forEach(([key, value]) => {
+					node.value = node.value.replaceAll(`%${key}%`, value);
+				});
+			}
+		});
+	};
+}
+export function rehypeReplaceVars() {
+	return (tree) => {
+		visit(tree, "element", (node) => {
+			if (node.properties) {
+				Object.entries(node.properties).forEach(([prop, val]) => {
+					if (typeof val === "string") {
+						Object.entries(replacements).forEach(([key, value]) => {
+							node.properties[prop] = val.replaceAll(`%${key}%`, value);
+						});
+					}
+				});
+			}
+		});
+		visit(tree, "mdxJsxFlowElement", (node) => {
+			if (node.attributes) {
+				Object.entries(node.attributes).forEach(([idx, attr]) => {
+					if (typeof attr.value === "string") {
+						Object.entries(replacements).forEach(([key, value]) => {
+							attr.value = attr.value.replaceAll(`%${key}%`, value);
+						});
+					}
+				});
+			}
+		});
+		visit(tree, "text", (node) => {
+			Object.entries(replacements).forEach(([key, value]) => {
+				node.value = node.value.replaceAll(`%${key}%`, value);
+			});
+		});
+	};
+}
