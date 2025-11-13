ef4b52d7631f79a3f5fba2075d997681d848ad37
Alessandro Pignotti <a.pignotti@sssup.it>
2025-11-13 22:38:15 +0100

    WIP - NO BUILD YET

--- sites/labs/astro.config.mjs
+++ sites/labs/astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from "astro/config";
+import icon from "astro-icon";
 import theme from "@leaningtech/astro-theme";
 import { resolve } from "node:path";
 
 // https://astro.build/config
 export default defineConfig({
 	site: "https://labs.leaningtech.com",
-	integrations: [theme()],
+	integrations: [icon(), theme()],
 	vite: {
 		resolve: {
 			alias: [
