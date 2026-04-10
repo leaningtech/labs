4a3b1490ac1558bb678b31ff274e8ff0f008be20
Yuri Iozzelli <y.iozzelli@gmail.com>
2026-04-10 14:37:53 +0200

    WIP bp 2.0

---
+++ packages/astro-theme/components/blog/BrowserPodTerminalMain.svelte
@@ -0,0 +1,28 @@
+<script>
+	import { BrowserPod } from "../../lib/browserpod.ts";
+	import {
+		BrowserPodEditorProvider,
+		TerminalPanel,
+		setAnalyticsCallback,
+	} from "@leaningtech/svelte-browserpod-editor";
+	import "@leaningtech/svelte-browserpod-editor/theme.css";
+
+	let { id, version, projectSource, apiKey, terminalTabs, ariaLabel } = $props();
+
+	// svelte-ignore state_referenced_locally
+	const pod = BrowserPod(version).boot({ apiKey });
+</script>
+
+<div
+	id={id}
+	class="flex w-full h-[25rem] p-2 bg-black/30 backdrop-blur-md border border-white/10 rounded-2xl"
+	aria-label={ariaLabel}
+>
+	<BrowserPodEditorProvider
+		{projectSource}
+		{pod}
+		ctxId="demo"
+	>
+		<TerminalPanel tabs={terminalTabs} />
+	</BrowserPodEditorProvider>
+</div>
