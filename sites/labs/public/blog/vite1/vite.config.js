ca23a988f8bb470f0bfaaef811c3ba6e63dcb003
Merlin de Cloe <89702955+codingfrog27@users.noreply.github.com>
2026-04-15 21:09:56 +0200

    matched versions with BP-svelte-editor (#385)

--- sites/labs/public/blog/vite1/vite.config.js
+++ sites/labs/public/blog/vite1/vite.config.js
@@ -4,4 +4,13 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [svelte()],
+	resolve: {
+		// Forcing Vite to bundle only one codemirror instance, else pnpm symlinks have a chance of linking twice, causing a singleton error
+		dedupe: [
+			"@codemirror/state",
+			"@codemirror/view",
+			"@codemirror/language",
+			"@codemirror/commands",
+		],
+	},
 });
