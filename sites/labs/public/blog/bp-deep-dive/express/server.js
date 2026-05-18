59464ccc91dd7763b12c2deea6198a660261f55f
Alessandro Pignotti <alessandro@leaningtech.com>
2026-05-18 10:15:51 +0200

    Appease the linter

--- sites/labs/public/blog/bp-deep-dive/express/server.js
+++ sites/labs/public/blog/bp-deep-dive/express/server.js
@@ -8,13 +8,13 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/api/hello", (req, res) => {
-  res.json({ message: "Hello from Express + BrowserPod!" });
+	res.json({ message: "Hello from Express + BrowserPod!" });
 });
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+	res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Express server listening on port ${PORT}`);
+	console.log(`Express server listening on port ${PORT}`);
 });
